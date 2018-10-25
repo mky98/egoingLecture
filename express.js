4308e9a8794d9b0841f0733ea5c42d7fd4eb85a9
Yubs <Yubs@naver.com>
2018-10-25 23:37:53 +0900

    5

--- express.js
+++ express.js
@@ -1,9 +1,13 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const fs = require('fs');
 var server = express();
 
 server.use(bodyParser.urlencoded({ extended: false }));
+server.use( cookieParser() );
+
+var authData = {username : 'yubs', userpassword : '1111', nickname : 'yeops'};
 
 function templateList(){
     var topics = fs.readdirSync('data');
@@ -16,7 +20,14 @@ function templateList(){
     return listTag;
 }
 
-function templateHTML(_listTag, _title, _desc){
+function templateHTML(_listTag, _title, _desc, _nickname){
+    var authUI = '';
+    if (_nickname){
+        authUI = '<a href="/logout">logout</a>';
+    }else{
+        authUI = '<a href="/logIn">logIn</a>';
+    }
+
     var content = `
             <!doctype html>
             <html>
@@ -25,6 +36,7 @@ function templateHTML(_listTag, _title, _desc){
                     <title>WEB</title>
                 </head>
                 <body>
+                    ${authUI}
                     <h1><a href="/">WEB</a></h1>
                     <ul>
                         ${_listTag}
@@ -38,18 +50,24 @@ function templateHTML(_listTag, _title, _desc){
     return content;
 }
 
-
 server.get('/', function (request, response) {
+
+    var username = request.cookies.username;
+    var userpassword = request.cookies.userpassword;
+    var nickname = null;
+    if (username === authData.username && userpassword === authData.userpassword){
+        nickname = authData.nickname;
+    }
+
     var title = 'Welcome';
     var desc = 'Hello, web';
     var listTag = templateList();
-    var content = templateHTML(listTag, title, desc);
+    var content = templateHTML(listTag, title, desc, nickname);
 
     response.write(content);
     response.end();
 });
 
-//http://localhost:3000/create_process?title=name&desc=aaaaa
 server.post('/create_process',function (request, response) {
     const title = request.body.title;
     const desc = request.body.desc;
@@ -57,13 +75,65 @@ server.post('/create_process',function (request, response) {
     response.redirect(`/topic/${title}`);
 });
 
+server.post('/login_process',function (request, response) {
+    const username = request.body.username;
+    const userpassword = request.body.userpassword;
+
+    if ( username === authData.username ){
+        console.log('아이디 맞음');
+        if ( userpassword === authData.userpassword ){
+            console.log('비번 맞음');
+            response.append('Set-Cookie', `username=${username}`);
+            response.append('Set-Cookie', `userpassword=${userpassword}`);
+            return response.redirect('/');
+        }
+    }
+
+    return response.send('누구?');
+});
+
+server.get('/cookie/set', function (request, response) {
+    response.cookie('foo','abc');
+    response.send('set cookies');
+});
+
+server.get('/cookie/get', function (request, response) {
+    console.log(request.cookies);
+    response.send('get cookies');
+});
+
 server.get('/create', function (request, response) {
+
+    var username = request.cookies.username;
+    var userpassword = request.cookies.userpassword;
+    var nickname = null;
+    if (username === authData.username && userpassword === authData.userpassword){
+        nickname = authData.nickname;
+    }else{
+        return response.redirect('/');
+    }
+
     var title = 'Create';
     var desc = `
     <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p><textarea name="desc" placeholder="description"></textarea></p>
         <p><input type="submit"></p>
+    </form>`;
+    var listTag = templateList();
+    var content = templateHTML(listTag, title, desc, nickname);
+
+    response.write(content);
+    response.end();
+});
+
+server.get('/login', function (request, response) {
+    var title = 'login';
+    var desc = `
+    <form action="/login_process" method="post">
+        <p><input type="text" name="username" placeholder="username"></p>
+        <p><input type="password" name="userpassword" placeholder="password"></textarea></p>
+        <p><input type="submit" value="login"></p>
     </form>`;
     var listTag = templateList();
     var content = templateHTML(listTag, title, desc);
@@ -74,12 +144,27 @@ server.get('/create', function (request, response) {
 
 
 server.get('/topic/:title', function (request, response) {
+
+    var username = request.cookies.username;
+    var userpassword = request.cookies.userpassword;
+    var nickname = null;
+    if (username === authData.username && userpassword === authData.userpassword){
+        nickname = authData.nickname;
+    }
+
     var title = request.params.title;
     var desc = fs.readFileSync('data/'+ title, 'utf8');
     var listTag = templateList();
-    var content = templateHTML(listTag, title, desc);
+    var content = templateHTML(listTag, title, desc, nickname);
 
     response.write(content);
     response.end();
 });
+
+server.get('/logout', function(request, response){
+    response.append('Set-Cookie', 'username=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'); 
+    response.append('Set-Cookie', 'userpassword=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'); 
+    response.redirect('/');
+});
+
 server.listen(3000);
